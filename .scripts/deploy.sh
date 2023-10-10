bde61a3988e733de7e07e7f7666881aec78caf41
Larval1 <t.merker@group24.de>
2023-10-10 08:21:34 +0200

    asdasda

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,5 +1,12 @@
-cd /var/www/clients/client1/web3/web/
+cd /var/www/clients/client1/web3/web/yopass
 
-mkdir test
+git pull
 
-cd ..
+cd website
+
+yarn install
+
+PUBLIC_URL='https://ots.group24.de' REACT_APP_BACKEND_URL='https://api.yopass.se' yarn build
+
+exit
+
